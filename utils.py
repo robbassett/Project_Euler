1a90bc79a5fc913254d4b7f012609b29c406a590
Robert Bassett <rbassett.astro@gmail.com>
2021-01-18 13:31:41 +1100

    first 10 done

--- utils.py
+++ utils.py
@@ -7,10 +7,26 @@ def fibonacci(Nterms):
         out[i+2] = out[i]+out[i+1]
     return out
 
-def primes(n):
+def primes_lt_n(n):
     """ Returns  a list of primes < n """
+    n = int(n)
     sieve = [True] * (n//2)
     for i in range(3,int(n**0.5)+1,2):
         if sieve[i//2]:
             sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
     return np.array([2] + [2*i+1 for i in range(1,n//2) if sieve[i]])
+
+def n_primes(n):
+    """ Returns first n primes """
+    p = [2]
+    c = 2
+    while len(p) < n:
+        j = 0
+        c += 1
+        while j < len(p):
+            if c % p[j] == 0:
+                break
+            elif j == len(p) - 1:
+                p.append(c)
+            j += 1
+    return p
