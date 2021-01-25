fde888e0d9fd42c7a01524e6540eee4d03527238
Robert Bassett <rbassett.astro@gmail.com>
2021-01-25 16:27:41 +1100

    21-30 done

--- third_10.py
+++ third_10.py
@@ -73,6 +73,93 @@ def euler25(digits):
             ind += 1
     return ind
 
+# PROJECT EULER 26
+def euler26(N):
+    mx = 0
+    ans = 0
+    for i in range(2,N+1):
+        done = False
+        t = 1
+        rems = []
+        while not done:
+            r = t%i
+            if r == 0:
+                done = True
+                recur = 0
+            else:
+                if r not in rems:
+                    rems.append(r)
+                    t = 10*r
+                else:
+                    done = True
+                    recur = len(rems)
+        if recur > mx:
+            mx = recur
+            ans = i
+    return ans
+
+# PROJECT EULER 27
+def euler27(mx):
+    def quad(n,a,b):
+        return n*n + n*a + b
+    
+    cprimes = primes_lt_n(1000000)
+    bprimes = primes_lt_n(mx)
+    mxl = 0
+    a0,b0 = 0,0
+    for a in range(-mx,mx):
+        for b in bprimes:
+            c = 0
+            n = 0
+            ns = []
+            ps = []
+            done = False
+            while not done:
+                c += 1
+                n += 1
+                if quad(n,a,b) in cprimes:
+                    ns.append(n)
+                    ps.append(quad(n,a,b))
+                else:
+                    done = True
+            if c > mxl:
+                mxl = c
+                a0,b0 = a,b
+    return a0*b0
+
+# PROJECT EULER 28
+def euler28(N):
+    out = 1
+    v = 1
+    for s in range(3,N+2,2):
+        for i in range(4):
+            v += (s-1)
+            out += v
+    return out
+
+# PROJECT EULER 29
+def euler29(lo,hi):
+    out = []
+    for a in range(lo,hi+1):
+        for b in range(lo,hi+1):
+            t = a**b
+            if t not in out: out.append(t)
+    return len(out)
+
+# PROJECT EULER 30:
+def euler30(P):
+    nums = []
+    ps = np.linspace(0,9,10)**P
+    done = False
+    st = 2
+    while not done:
+        tm = np.array([ps[int(i)] for i in str(st)]).sum()
+        if st == tm: nums.append(st)
+        if st > 200000:
+            done = True
+        st+=1
+    return np.sum(nums)
+
 if __name__ == '__main__':
     # TWENTY ONE:
     st = time.time()
@@ -93,5 +180,24 @@ if __name__ == '__main__':
     # TWENTY FIVE:
     st = time.time()
     print(f'Problem 25: {euler25(1000)} ({time.time()-st} s)')
+    
+    # TWENTY SIX:
+    st = time.time()
+    print(f'Problem 26: {euler26(1000)} ({time.time()-st} s)')
+    
+    # TWENTY SEVEN:
+    st = time.time()
+    print(f'Problem 27: {euler27(1000)} ({time.time()-st} s)')
 
+    # TWENTY EIGHT:
+    st = time.time()
+    print(f'Problem 28: {euler28(1001)} ({time.time()-st} s)')
+
+    # TWENTY NINE:
+    st = time.time()
+    print(f'Problem 29: {euler29(2,100)} ({time.time()-st} s)')
+
+    # THIRTY:
+    st = time.time()
+    print(f'Problem 30: {euler30(5)} ({time.time()-st} s)')
     
