367d5eb310446a0e25d28c619ecec43fbdead081
Rob Bassett <rbassett.astro@gmail.com>
2021-11-12 11:06:57 +1100

    euler8 js

--- js/euler1to10.js
+++ js/euler1to10.js
@@ -1,6 +1,7 @@
 const performance = require('perf_hooks').performance;
 
 utils = require('./utils.js')
+data = require('./data.js')
 
 function euler1(mx){
     var t0 = performance.now()
@@ -99,6 +100,19 @@ function euler7(n){
     console.log('Euler #7: ' + ans + '  (' + (t1-t0) + ' ms)')
 }
 
+function euler8(n){
+    var t0 = performance.now()
+    var str = data.euler8
+    var ans = 0
+    for (i = 0; i < str.length - n; i++) {
+        var tp = parseInt(str[i])
+        for (j = i+1; j < i+n; j++) {tp *= str[j]}
+        if (tp > ans) {ans = tp}
+    }
+    var t1 = performance.now()
+    console.log('Euler #8: ' + ans + ' (' + (t1-t0) + ' ms)')
+}
+
 euler1(1000)
 euler2(4000000)
 euler3(600851475143)
@@ -106,3 +120,4 @@ euler4(3)
 euler5(20)
 euler6(100)
 euler7(10001)
+euler8(13)
