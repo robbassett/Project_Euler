256b18c3912ea14be7dcc18e54cc410e0091fdc1
Rob Bassett <rbassett.astro@gmail.com>
2022-05-25 11:56:40 +1000

    23 js

--- js/euler21to30.js
+++ js/euler21to30.js
@@ -36,5 +36,28 @@ const euler22 = function() {
     console.log('Euler #22: ' + ans + ' (' + (t1-t0) + ' ms)')
 }
 
+const euler23 = function() {
+    t0 = performance.now();
+    var abund_nums = [];
+    var ans = 0
+    for (let i = 12; i < 28123; i++) {
+        if (utils.get_factors(i).reduce((p,a) => p + a, 0)-i > i) {abund_nums.push(i)}
+    }
+    for (let i = 0; i < 28123; i++) {
+        var flag = 0
+        for (let j = 0; j < abund_nums.length; j++) {
+            if (abund_nums[j] > i/2) {break};
+            if (abund_nums.includes(i-abund_nums[j])) {
+                flag = 1;
+                break
+            };
+        }
+        if (flag == 0) {ans+=i}
+    }
+    t1 = performance.now()
+    console.log('Euler #23: ' + ans + ' (' + (t1-t0) + ' ms)')
+}
+
 euler21()
 euler22()
+euler23()
