38c05bbf47748aa6210f1d7c6a713b3692a84f4d
Rob Bassett <rbassett.astro@gmail.com>
2022-08-30 09:45:25 +1000

    testing e113

--- python/twelfth_10.py
+++ python/twelfth_10.py
@@ -7,7 +7,7 @@ def euler111():
     return 'Not solved'
 
 # This one is probably way longer than necessary...
-# was going to do some clever stuff to optimise, but it's not that slow so whatever
+# was going to do some clever stuff to optimise, but it's not that slow so whatever (see 113)
 def euler112(target=0.5):
     def add_one(n):
         ind = -1
@@ -57,6 +57,78 @@ def euler112(target=0.5):
     ans = ''
     for _ in n: ans+=str(_)
     return int(ans)
+
+def euler113(target=5):
+    def add_one(n):
+        ind = -1
+        while True:
+            if n[ind] != 9:
+                n[ind]+=1
+                break
+            else:
+                n[ind] = 0
+                ind -= 1
+        return n
+
+    def n_to_str(n):
+        out = str(n[0])
+        for _ in range(len(n)-1): out+='0'
+        return out
+
+    def n_to_str(n):
+        out = str(n[0])
+        i = 1
+        while i < len(n)-2:
+            out+=str(n[i])
+            i+=1
+        while len(out) < len(n):
+            out += '0'
+        return out
+
+    n = [1,0,0]
+    b = 0
+    record = {}
+    while len(n) < target:
+        if all([_ == 9 for _ in n]):
+            n = [1] + [0]*len(n)
+        n = add_one(n)
+        i = 0
+        sd = None
+        while True:
+            if n[i] == n[i+1]:
+                i+=1
+                if i == len(n)-1: break
+                continue
+            sd = 'd' if n[i] > n[i+1] else 'u'
+            si = i
+            break
+        if not sd:
+            continue
+        
+        i+=1
+        bounce = False
+        while True:
+            if i >= len(n)-1: break
+            if n[i] == n[i+1]:
+                i+=1 
+                if i == len(n)-1: break
+                continue
+            nd = 'd' if n[i] > n[i+1] else 'u'
+            if sd != nd:
+                bounce = True
+                break
+            i+=1
+        if bounce: 
+            b+=1
+            sn = n_to_str(n)
+            if sn not in record.keys():
+                record[sn] = 1
+            else:
+                record[sn] += 1
+
+    ans = ''
+    for _ in n: ans+=str(_)
+    return record
 
 def euler120():
     ans = 0
