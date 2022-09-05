f45f9dbb3d8e9f4902fe30d04bf00b6089b0d20f
Rob Bassett <rbassett.astro@gmail.com>
2022-09-05 14:16:18 +1000

    113 python

--- python/twelfth_10.py
+++ python/twelfth_10.py
@@ -58,77 +58,13 @@ def euler112(target=0.5):
     for _ in n: ans+=str(_)
     return int(ans)
 
-def euler113(target=5):
-    def add_one(n):
-        ind = -1
-        while True:
-            if n[ind] != 9:
-                n[ind]+=1
-                break
-            else:
-                n[ind] = 0
-                ind -= 1
-        return n
+def euler113():
+    def combs(n,r):
+        num = utils.factorial(n)
+        den = utils.factorial(r)*utils.factorial(n-r)
+        return num/den
 
-    def n_to_str(n):
-        out = str(n[0])
-        for _ in range(len(n)-1): out+='0'
-        return out
-
-    def n_to_str(n):
-        out = str(n[0])
-        i = 1
-        while i < len(n)-2:
-            out+=str(n[i])
-            i+=1
-        while len(out) < len(n):
-            out += '0'
-        return out
-
-    n = [1,0,0]
-    b = 0
-    record = {}
-    while len(n) < target:
-        if all([_ == 9 for _ in n]):
-            n = [1] + [0]*len(n)
-        n = add_one(n)
-        i = 0
-        sd = None
-        while True:
-            if n[i] == n[i+1]:
-                i+=1
-                if i == len(n)-1: break
-                continue
-            sd = 'd' if n[i] > n[i+1] else 'u'
-            si = i
-            break
-        if not sd:
-            continue
-        
-        i+=1
-        bounce = False
-        while True:
-            if i >= len(n)-1: break
-            if n[i] == n[i+1]:
-                i+=1 
-                if i == len(n)-1: break
-                continue
-            nd = 'd' if n[i] > n[i+1] else 'u'
-            if sd != nd:
-                bounce = True
-                break
-            i+=1
-        if bounce: 
-            b+=1
-            sn = n_to_str(n)
-            if sn not in record.keys():
-                record[sn] = 1
-            else:
-                record[sn] += 1
-
-    ans = ''
-    for _ in n: ans+=str(_)
-    return record
+    return int(combs(110,10) + combs(109,9) - 2 - 10*100)
 
 def euler120():
     ans = 0
@@ -147,6 +83,10 @@ if __name__ == '__main__':
     # ONE HUNDRED AND TWELVE:
     st = time.time()
     print(f'Problem 112: {euler112(.99)} ({time.time()-st} s)')
+    
+    # ONE HUNDRED AND THIRTEEN:
+    st = time.time()
+    print(f'Problem 113: {euler113()} ({time.time()-st} s)')
     
     # ONE HUNDRED AND TWENTY:
     st = time.time()
